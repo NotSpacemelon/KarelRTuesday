e244e49dbe9aa783e2821cd3af1539772d195c88
Mark Lessard <nlessard@mon-avenir.ca>
2023-06-05 12:08:24 -0400

    tache9a

---
+++ tasks/tache9a.rb
@@ -0,0 +1,8 @@
+#Tache9a
+
+num1 = rand(0..20)
+if num1 < 10 then 
+	puts "Ta valeur est #{num1}, et est inferieur a 10"
+elsif num1 > 10 then 
+	puts "Ta valeur est #{num1}, et est superieur a 10"
+end
---
+++ tasks/tache9aa.rb
@@ -0,0 +1,9 @@
+#Tache9a
+
+num1 = rand(0..20)
+num2 = rand(0..20)
+if num1 < num2 then 
+	puts "Tes valeurs sont #{num1} et #{num2}, et #{num2} est superieur"
+elsif num1 > num2 then 
+	puts "Tes valeurs sont #{num1} et #{num2}, et #{num1} est superieur"
+end
